f37b9d9795c9f89519954f60bcbbaae56db518ea
psycd8 <psycd8@exmail.nottingham.ac.uk>
2022-10-14 21:23:48 +0100

    add buttons to gui

--- calculator/src/com/calc/Window.java
+++ calculator/src/com/calc/Window.java
@@ -2,10 +2,12 @@ package com.calc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Window {
-    private JFrame guiFrame = new JFrame();
-    private JLabel title = new JLabel("Hello, world!");
+    private final JFrame guiFrame = new JFrame();
+    private final ArrayList<JButton> buttArr = new ArrayList<>();
+
 
     public Window() {
         guiFrame.setTitle("Calculator");
@@ -14,10 +16,33 @@ public class Window {
     public void createGUI() {
         guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         guiFrame.setLayout(new BorderLayout());
-        guiFrame.setPreferredSize(new Dimension(400,800));
+        guiFrame.setPreferredSize(new Dimension(400,700));
 
         JPanel panel = new JPanel();
-        panel.add(title);
+        panel.setLayout(new FlowLayout());
+        for(int i = 0; i < 20; i++) {
+            String buttText = switch (i) {
+                case 0 -> "B";
+                case 1 -> "(";
+                case 2 -> ")";
+                case 3 -> "C";
+                case 7 -> "/";
+                case 11 -> "*";
+                case 15 -> "-";
+                case 16 -> "0";
+                case 17 -> ".";
+                case 18 -> "=";
+                case 19 -> "+";
+                default -> "" + i;
+            };
+
+            buttArr.add(new JButton(buttText));
+            if(i == 9) {
+                buttArr.get(i).setText("0");
+            }
+            buttArr.get(i).setPreferredSize(new Dimension(100,100));
+            panel.add(buttArr.get(i));
+        }
 
         guiFrame.add(panel, BorderLayout.CENTER);
 
